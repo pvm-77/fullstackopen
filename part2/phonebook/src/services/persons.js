fd7c3e3205882d645ad2cf4a25b3f60183be9f1f
pvm-77 <sfzalig@gmail.com>
2022-10-05 16:05:49 +0530

    part2 phonebook pacakage.json configure

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 const getAll=()=>axios.get(baseUrl)
 const create = newObject => axios.post(baseUrl, newObject)
 const deletePerson = id => axios.delete(`${baseUrl}/${id}`)
