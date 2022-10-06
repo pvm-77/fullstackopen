713e963d93c66a4c1f3162194a576973a33e6006
pvm-77 <sfzalig@gmail.com>
2022-10-06 16:48:15 +0530

    fullstack phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,54 +5,90 @@ const Person = require('./models/person')
 app.use(express.json())
 const morgan = require('morgan')
 const cors = require('cors')
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
 app.use(cors())
 app.use(express.static('build'))
+
 // fetching a singlec resource
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      // res.json(person)
+      res.status(200).json(person)
+    } else {
+      res.status(404).json({ error: 'person not found' })
+    }
+  }).catch(e => next(e))
 })
 // create request
-app.post('/api/persons', (req, res) => {
-    if (req.body.name === '' || req.body.name === undefined) {
-        return res.status(404).json({ error: 'name  is missing' })
-    }
-    if (req.body.number === '' || req.body.number === undefined) {
-        return res.status(404).json({ error: ' number is missing' })
-    }
-    // person object to create new record 
-    const person = new Person({
-        name: req.body.name,
-        number: req.body.number
-    })
-    person.save().then(savedPerson => {
-        res.status(200).json(savedPerson)
-    })
+app.post('/api/persons', (req, res, next) => {
+  if (req.body.name === '' || req.body.name === undefined) {
+    return res.status(404).json({ error: 'name  is missing' })
+  }
+  if (req.body.number === '' || req.body.number === undefined) {
+    return res.status(404).json({ error: ' number is missing' })
+  }
+  // person object to create new record
+  const person = new Person({
+    name: req.body.name,
+    number: req.body.number
+  })
+  person.save().then(savedPerson => {
+    res.status(200).json(savedPerson)
+  }).catch(error => next(error)
+
+  )
 })
 // get all persons
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.status(200).json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.status(200).json(persons)
+  })
 })
 app.get('/info', (req, res) => {
-    res.send(`<div>phonebook has info of 2 people <p>${new Date()}</p></div>`)
+  Person.find({}).count().then(count => {
+    res.send(`<div>phonebook has info of ${count} people <p>${new Date()}</p></div>`)
+  })
+
 })
 // delete by id
-app.delete('/api/persons/:id', (req, res) => {
-    Person.findByIdAndDelete(req.params.id).then(deletePerson => {
-        res.status(204).end();
-    })
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id).then(() => {
+    res.status(204).end()
+  }).catch(e => next(e))
+})
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number
+  }
+  console.log(person)
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      res.status(204).json(updatedPerson)
+    }).catch(e => next(e))
 })
 
+
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
+  console.log(`server is running on port ${PORT}`)
 })
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -3,25 +3,39 @@ const url = process.env.MONGODB_URI
 // console.log(`connecting to ${url}`);
 
 mongoose
-    .connect(url)
-    .then(result => {
-        console.log('connected to mongodb');
-    })
-    .catch(error => {
-        console.log('error connecting to mongodb', error.message);
-    })
+  .connect(url)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(error => {
+    console.log('error connecting to mongodb', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (myNumber) {
+        return /^\d{2,3}-\d{5,}$/.test(myNumber)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true,
+    minLength:8
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
