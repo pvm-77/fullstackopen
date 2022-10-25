1b2affe835314d6add19d2934669315178a23626
pvm-77 <sfzalig@gmail.com>
2022-10-25 15:13:58 +0530

    merge to main

---
+++ part5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const baseUrl = '/api/login'
+
+const login = async (credentials) => {
+    console.log(credentials)
+    const response = await axios.post(baseUrl, credentials)
+    console.log(response.data);
+    return response.data;
+}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { login }
