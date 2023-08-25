a9298ccbb1bc0aecffe1193786eba736c02104ad
pvm-77 <sfzalig@gmail.com>
2023-08-25 12:32:07 +0530

    complete 9.14 solution

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -1,10 +1,40 @@
-import Header from "./components/Header";
-import Content from "./Content";
-import Total from "./components/Total";
+interface HeaderProps {
+  name: string;
+}
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+interface TotalProps {
+  courseParts: CoursePart[];
+}
+const Header = (props: HeaderProps) => {
+  return <h1>{props.name}</h1>;
+};
+const Total = (props: TotalProps) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  );
+};
 
+const Content = (props: ContentProps) => {
+  console.log(props.courseParts)
+
+  return(
+    <div>{
+      props.courseParts.map(p=><p key={p.name}>{p.name}  {p.exerciseCount}</p>)
+    }</div>
+  )
+};
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
@@ -21,8 +51,9 @@ const App = () => {
 
   return (
     <div>
-      <Header courseName={courseName} />
+      <Header name={courseName} />
       <Content courseParts={courseParts} />
+
       <Total courseParts={courseParts} />
     </div>
   );
